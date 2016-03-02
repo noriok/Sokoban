c747c4f6af6dc91ddea4c3cf3977db1f00c0b53b
Norio Kobayashi <noriokob@gmail.com>
2016-03-02 16:03:24 +0900

    Load stage data form text file.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GameManager {
 
     public GameManager() {
-
     }
 
     public List<string> NextStage() {
-        var stage = new List<string>();
-        stage.Add("#######");
-        stage.Add("#.....#");
-        stage.Add("#.xo..#");
-        stage.Add("#.p...#");
-        stage.Add("#.....#");
-        stage.Add("#######");
-        return stage;
+        string filename = "Stage/stage1";
+        return ReadStageData(filename);
 	}
+
+    private List<string> ReadStageData(string filename) {
+        TextAsset t = Resources.Load<TextAsset>(filename);
+        string[] ss = t.text.Split(new[] { '\n' });
+        return ss.ToList();
+    }
 }
