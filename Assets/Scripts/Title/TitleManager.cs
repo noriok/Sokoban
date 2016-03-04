0ddc340eeff95f0a9f392959eb22dabbd9188a38
Norio Kobayashi <noriokob@gmail.com>
2016-03-04 12:33:40 +0900

    Add Title Scene.

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour {
+
+    void Start() {
+        var button = GameObject.Find("Button");
+        button.GetComponent<Button>().onClick.AddListener(() => {
+            SceneManager.LoadScene("Game");
+
+        });
+    }
+
+    void Update() {
+    }
+}
